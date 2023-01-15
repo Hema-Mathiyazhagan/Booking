6866f67cafe218fa5e8d34863c05e3ed0b991733
Hema-Mathiyazhagan <32691880+Hemamathi@users.noreply.github.com>
2023-01-15 11:11:46 +0530

    changed

--- src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
+++ src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
@@ -1,7 +1,9 @@
 import com.booking.recruitment.hotel.model.Hotel;
 import com.booking.recruitment.hotel.service.HotelService;
+import com.booking.recruitment.hotel.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,40 +11,43 @@ import java.util.List;
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
-  private final HotelService hotelService;
+    private final HotelService hotelService;
 
-  @Autowired
-  public HotelController(HotelService hotelService) {
-    this.hotelService = hotelService;
-  }
+    @Autowired
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
 
-  @GetMapping
-  @ResponseStatus(HttpStatus.OK)
-  public List<Hotel> getAllHotels() {
-    return hotelService.getAllHotels();
-  }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Hotel> getAllHotels() {
+        return hotelService.getAllHotels();
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Hotel createHotel(@RequestBody Hotel hotel) {
-    return hotelService.createNewHotel(hotel);
-  }
-  
-  @GetMapping("/{id}")
-  @ResponseBody
-  public Hotel getHotelbyHotelId(@PathVariable("id")Long id) {
-	  return hotelService.getHotelbyHotelId(id);
-  }
-  
-  @PatchMapping(value="/{id}")
-  @ResponseBody
-  public void deleteHotelbyHotelId(@PathVariable("id") Long id) {
-	  hotelService.deleteHotelbyHotelId(id);
-  }
-  
-  @GetMapping(value="/search/{cityId}")
-  @ResponseBody
-  public List<Hotel> findingBestHotelsforCustomers(@PathVariable("cityId")Long cityId,@RequestParam("sortBy") Double distance){
-	  return hotelService.findingBestHotelsforCustomers(cityId, distance);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Hotel createHotel(@RequestBody Hotel hotel) {
+        return hotelService.createNewHotel(hotel);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> getHotelbyHotelId(@PathVariable("id")Long id) {
+        Hotel hotel=hotelService.getHotelbyHotelId(id);
+        return new ResponseEntity<>(hotel,HttpStatus.OK);
+    }
+
+    @PatchMapping(value="/{id}")
+    @ResponseBody
+    public ResponseEntity<?>  deleteHotelbyHotelId(@PathVariable("id") Long id) {
+        hotelService.deleteHotelbyHotelId(id);
+        return new ResponseEntity<>("Deleted",HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping(value="/search/{cityId}")
+    @ResponseBody
+    public ResponseEntity<?> findingBestHotelsforCustomers(@PathVariable("cityId")Long cityId,@RequestParam("sortBy") Double distance){
+        List<Hotel>list= hotelService.findingBestHotelsforCustomers(cityId, distance);
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
 }
